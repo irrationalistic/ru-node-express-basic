8ea22fe7ff93616dbb124a91d9e84df43de8cb99
Chris Rolfs <chris@refactoru.com>
2014-05-05 15:43:05 -0600

    Added default port to app

--- app.js
+++ app.js
@@ -11,5 +11,8 @@ app.get('/', function(req, res){
 	});
 });
 
-app.listen(3000);
-console.log('Server listening at localhost:3000');
+var port = process.env.PORT || 3000;
+app.listen(port);
+console.log('Server listening at port ' + port);
+
+
